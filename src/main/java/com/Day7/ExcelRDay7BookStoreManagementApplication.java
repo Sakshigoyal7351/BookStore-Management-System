3ba0e01c140dd90b8ddc8f8a2b335b4112c90388
sakshi <sakshigoyal7351@gmail.com>
2023-08-08 17:52:40 +0530

    first commit

---
+++ src/main/java/com/Day7/ExcelRDay7BookStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Day7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelRDay7BookStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelRDay7BookStoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Day7/ExcelRDay7BookStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Day7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelRDay7BookStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
